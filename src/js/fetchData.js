bd837df77e6cd13b7f3d0101f5c25eafa99d2d13
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-11-29 00:35:06 +0200

    Create fetchData function. Add manual for the fetchData function.

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
-const fetchData = async (queryType, params) => {
+export const fetchData = async (queryType, params) => {
   switch (queryType) {
     case 'TRENDING':
       const trendingLink = 'https://api.themoviedb.org/3/trending/movie/week';
       try {
         const data = await axios.get(trendingLink, {
-          params: { api_key: 'a8c13239d5351cd341496e4bdbeed27b', ...params },
+          params: {
+            api_key: 'a8c13239d5351cd341496e4bdbeed27b',
+            include_adult: false,
+            ...params,
+          },
         });
         console.log('TRENDING');
         return data;
@@ -15,24 +19,78 @@ const fetchData = async (queryType, params) => {
       }
       break;
 
-    case 'SEARCH_FILMS':
-      const searchLink = 'https://api.themoviedb.org/3/trending/movie/week';
+    case 'SEARCH_MOVIES':
+      const searchLink = 'https://api.themoviedb.org/3/search/movie';
       try {
         const data = await axios.get(searchLink, {
-          params: { api_key: 'a8c13239d5351cd341496e4bdbeed27b', ...params },
+          params: {
+            api_key: 'a8c13239d5351cd341496e4bdbeed27b',
+            include_adult: false,
+            ...params,
+          },
         });
-        console.log('SEARCH_FILMS');
         return data;
       } catch (error) {
         console.error(error.message);
       }
       break;
 
+    case 'FIND_MOVIE':
+      try {
+        const findFilmLink = `https://api.themoviedb.org/3/movie/${params.id}`;
+        const data = await axios.get(findFilmLink, {
+          params: {
+            api_key: 'a8c13239d5351cd341496e4bdbeed27b',
+            include_adult: false,
+          },
+        });
+        return data;
+      } catch (error) {
+        console.error(
+          error.message,
+          '; You should pass object with "id" element as the second parameter'
+        );
+      }
+      break;
+
+    case 'FIND_MOVIE_VIDEO':
+      try {
+        const findFilmLink = `https://api.themoviedb.org/3/movie/${params.id}/videos`;
+        const data = await axios.get(findFilmLink, {
+          params: {
+            api_key: 'a8c13239d5351cd341496e4bdbeed27b',
+            include_adult: false,
+          },
+        });
+        return data;
+      } catch (error) {
+        console.error(
+          error.message,
+          '; You should pass object with "id" element as the second parameter'
+        );
+      }
+      break;
     default:
       console.error('Such query type does not exist');
   }
 };
-export default fetchData;
 
-const data = fetchData('SEARCH_FILMS');
-console.log(data);
+// Query types
+export const TRENDING = 'TRENDING';
+export const SEARCH_MOVIES = 'SEARCH_MOVIES';
+export const FIND_MOVIE = 'FIND_MOVIE';
+export const FIND_MOVIE_VIDEO = 'FIND_MOVIE_VIDEO';
+
+// How to use
+
+// const data = fetchData(TRENDING, { page: 3 });
+// console.log(data);
+
+// const data = fetchData(SEARCH_MOVIES, { page: 1, query: 'King Art' });
+// console.log(data);
+
+// const data = fetchData(FIND_MOVIE, { id: 11 });
+// console.log(data);
+
+// const data = fetchData(FIND_MOVIE_VIDEO, { id: 11 });
+// console.log(data);
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 import axios from 'axios';
 import { Pagination } from 'tui-pagination';
-import fetchData from './fetchData';
+import {
+  fetchData,
+  FIND_MOVIE,
+  FIND_MOVIE_VIDEO,
+  SEARCH_MOVIES,
+  TRENDING,
+} from './fetchData';
