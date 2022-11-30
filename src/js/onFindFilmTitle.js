afde5dd0a2e0f6d6ff232bd3bdddb6250ac58e64
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-11-30 13:21:25 +0200

    add loader to the page

--- src/js/onFindFilmTitle.js
+++ src/js/onFindFilmTitle.js
@@ -1,31 +1,35 @@
-import {onRenderGallery} from './onRenderGallery';
-import {fetchData,SEARCH_MOVIES} from './fetchData';
+import { onRenderGallery } from './onRenderGallery';
+import { fetchData, SEARCH_MOVIES } from './fetchData';
+import { loading } from './loading';
 
 export const onFindFilmTitle = () => {
-    const form = document.querySelector('.header__form');
-    let query;
-    const errorField = document.querySelector('.header-error__text');
-    
-    form.addEventListener('submit', onSubmit)
+  const form = document.querySelector('.header__form');
+  let query;
+  const errorField = document.querySelector('.header-error__text');
 
-    async function onSubmit(e) {
-        e.preventDefault();
-        errorField.textContent = '';
+  form.addEventListener('submit', onSubmit);
 
-        const {searchQuery} = e.currentTarget;
-            if (query === searchQuery.value) {
-                return
-            }
-        query = searchQuery.value;
+  async function onSubmit(e) {
+    e.preventDefault();
+    const gallery = document.querySelector('.film-selection');
+    gallery.innerHTML = loading;
+    errorField.textContent = '';
 
-        const res = await fetchData(SEARCH_MOVIES, { page: 1, query });
-
-        if (res.data.results.length === 0) {
-            errorField.textContent = "There is no films with such name. Please, try again."
-            return;
-        }
-        onRenderGallery(res.data.results);
+    const { searchQuery } = e.currentTarget;
+    if (query === searchQuery.value) {
+      return;
     }
-}
+    query = searchQuery.value;
+
+    const res = await fetchData(SEARCH_MOVIES, { page: 1, query });
+
+    if (res.data.results.length === 0) {
+      errorField.textContent =
+        'There is no films with such name. Please, try again.';
+      return;
+    }
+    onRenderGallery(res.data.results);
+  }
+};
 
 onFindFilmTitle();
