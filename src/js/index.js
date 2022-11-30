115d3771a65257ec15daf2e5047b72e2fdd66cdf
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-11-30 02:35:27 +0200

    Add modal Window

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,38 @@
 import axios from 'axios';
 import { Pagination } from 'tui-pagination';
-import {
-  fetchData,
-  FIND_MOVIE,
-  FIND_MOVIE_VIDEO,
-  SEARCH_MOVIES,
-  TRENDING,
-} from './fetchData';
 import { onFindFilmTitle } from './onFindFilmTitle';
+
+import { onCloseModal } from './modal-window';
+
+import { fetchData, FIND_MOVIE } from './fetchData';
+const FILMS_LIST = document.querySelector('.film-selection');
+
+const renderFilm = async id => {
+  const MODAL_WINDOW = document.querySelector('.modal-window');
+  const PICTURE = MODAL_WINDOW.querySelector('.detailed-info__image');
+  const TITLE = MODAL_WINDOW.querySelector('.detailed-info__caption');
+  const ABOUT = MODAL_WINDOW.querySelector('.about__text');
+
+  console.log(PICTURE);
+  const filmData = await fetchData(FIND_MOVIE, { id });
+  console.log(filmData.data);
+  const { poster_path, overview, original_title } = filmData.data;
+
+  const imgUrl = 'https://image.tmdb.org/t/p/w500' + poster_path;
+
+  PICTURE.src = imgUrl;
+  TITLE.textContent = original_title;
+  ABOUT.textContent = overview;
+
+  MODAL_WINDOW.classList.remove('hidden');
+};
+
+const openModalWindow = event => {
+  if (event.target.className === 'film-selection') return;
+  const film = event.target.closest('.film');
+  const filmId = film.dataset.id;
+  console.log(filmId);
+  renderFilm(filmId);
+};
+
+FILMS_LIST.addEventListener('click', openModalWindow);
--- src/js/onRenderGallery.js
+++ src/js/onRenderGallery.js
@@ -1,25 +1,24 @@
-export const onRenderGallery = (films) => {
-    const gallery = document.querySelector('.film-selection');
-    let imgUrl = '';
-    gallery.innerHTML = '';
-    const markup = films
+export const onRenderGallery = films => {
+  const gallery = document.querySelector('.film-selection');
+  let imgUrl = '';
+  gallery.innerHTML = '';
+  const markup = films
     .map(film => {
-        const {poster_path, title, release_date} = film;
-        if (poster_path === null) {
-            imgUrl = "https://via.placeholder.com/700?text=NoImageFound"
-        } else {
-            imgUrl = "https://image.tmdb.org/t/p/w500" + poster_path;
-        }
-        
-     return `
-     <article class="film">
+      const { poster_path, title, release_date, id } = film;
+      if (poster_path === null) {
+        imgUrl = 'https://via.placeholder.com/700?text=NoImageFound';
+      } else {
+        imgUrl = 'https://image.tmdb.org/t/p/w500' + poster_path;
+      }
+
+      return `
+     <article class="film" data-id=${id}>
         <img class="film__poster" src=${imgUrl} alt="" />
         <h2 class="film__title">${title}</h2>
         <p class="film__info">Drama, Action | ${release_date}</p>
      </article>
-     `
-        
-        })
-    .join("")
-    gallery.insertAdjacentHTML('beforeend', markup)
-}
+     `;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+};
