ee799f58fd9ecf77172d94ac2a66b40b83cf9327
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-12-05 00:27:45 +0200

    Add localstorage user data

--- src/js/firebase/index.js
+++ src/js/firebase/index.js
@@ -34,11 +34,9 @@ const getUserData = async () => {
       const token = credential.accessToken;
       // The signed-in user info.
       const user = jwtDecode(result.user.accessToken);
-      console.log(user);
       localStorage.setItem('user', JSON.stringify(user));
-      const USERINFO = document.querySelector('.user-info');
-      console.log(USERINFO);
-      USERINFO.innerHTML = `<p class="user-info__name"> ${user.name}</p>
+      const USER_INFO = document.querySelector('.user-info');
+      USER_INFO.innerHTML = `<p class="user-info__name"> ${user.name}</p>
       <img class="user-info__image"
         src=${user.picture}
         alt="${user.name}">`;
@@ -56,4 +54,16 @@ const getUserData = async () => {
     });
 };
 
-login.addEventListener('click', getUserData);
+const loginOperation = () => {
+  if (JSON.parse(localStorage.getItem('user'))) {
+    localStorage.removeItem('user');
+    const USER_INFO = document.querySelector('.user-info');
+    const LOGIN_BUTTON = document.querySelector('.login-button');
+    USER_INFO.innerHTML = '';
+    LOGIN_BUTTON.textContent = 'LOGIN';
+    return;
+  }
+  getUserData();
+};
+
+login.addEventListener('click', loginOperation);
