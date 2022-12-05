ea599b6722c6f355fe6ee05045c798f170e76e7a
Kaka8548 <katyakatya8548@gmail.com>
2022-12-05 04:46:49 +0400

    Bag fixed
     - pagination fixed
     - footer modal fixed (adding and removing visually-hidden class)
     - the text in the library page corrected

--- src/js/library.js
+++ src/js/library.js
@@ -1,6 +1,7 @@
 import { onRederLibrary } from './utilities/onRenderLibrary';
 import { openModalWindow } from './modal-window';
 import { libraryEventListeners } from './utilities/onRenderLibrary';
+import './utilities/paginationLib';
 
 onRederLibrary('watched', 1);
 libraryEventListeners();
